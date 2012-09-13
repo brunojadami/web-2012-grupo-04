3db52e13bfbb178024e41fd3b9a9446a1131cc2c
brunojadami@gmail.com <brunojadami@gmail.com@099c295c-9551-93b2-87df-dac76434ca7d>
2012-09-13 20:35:23 +0000

    Updated some js validations.

--- WebProject/web/script/complementary.js
+++ WebProject/web/script/complementary.js
@@ -7,14 +7,14 @@ function validateForm()
     
     // Nota: todos os campos do tipo 'select' possuem a validação /(*)$/i. Eles só são validados no servidor.
     return validate(form["generalLevel"].value, null, "Nível inválido.")
-        && validate(form["generalInstitution"].value, /[a-z ]{4,40}$$/i, "Instituição inválida.")
-        && validate(form["generalCourse"].value, /[a-z ]{4,40}$/i, "Curso inválido.")
+        && validate(form["generalInstitution"].value, /^[a-z ]{4,40}$$/i, "Instituição inválida.")
+        && validate(form["generalCourse"].value, /^[a-z ]{4,40}$/i, "Curso inválido.")
         && validate(form["generalStatus"].value, null, "Estado inválido.")
-        && validate(form["generalTime"].value, /[0-9]{1,2}$/i, "Carga horária inválida.")
+        && validate(form["generalTime"].value, /^[0-9]{1,2}$/i, "Carga horária inválida.")
     
-        && validate(form["periodBegin"].value, /[0-9]{2,4}$/i, "Ano de início inválido.")
-        && validate(form["periodEnd"].value, /[0-9]{2,4}$/i, "Ano de término inválido.")
+        && validate(form["periodBegin"].value, /^[0-9]{2,4}$/i, "Ano de início inválido.")
+        && validate(form["periodEnd"].value, /^[0-9]{2,4}$/i, "Ano de término inválido.")
         
         && validate(form["scholarshipPresence"].value, null, "Com bolsa? inválido.")
-        && validate(form["scholarshipAgency"].value, /[a-z ]{4,40}$/i, "Agência inválida.");
+        && validate(form["scholarshipAgency"].value, /^[a-z ]{4,40}$/i, "Agência inválida.");
 }
--- WebProject/web/script/experience.js
+++ WebProject/web/script/experience.js
@@ -6,16 +6,16 @@ function validateForm()
     var form = document.forms["form"];
     
     // Nota: todos os campos do tipo 'select' possuem a validação /(*)$/i. Eles só são validados no servidor.
-    return validate(form["profInstitution"].value, /[a-z ]{4,40}$/i, "Instituição inválida.")
+    return validate(form["profInstitution"].value, /^[a-z ]{4,40}$/i, "Instituição inválida.")
         
-	&& validate(form["bondType"].value, /[a-z ]{4,40}$/i, "Tipo de vínculo inválido.")
+	&& validate(form["bondType"].value, /^[a-z ]{4,40}$/i, "Tipo de vínculo inválido.")
         && validate(form["bondEmployment"].value, null, "Vínculo empregatício inválido.")
-        && validate(form["bondFunctional"].value, /[a-z ]{4,40}$/i, "Enquadramento funcional inválido.")
-	&& validate(form["bondTime"].value, /[0-9]{1,2}$/i, "Carga horária inválida.")
+        && validate(form["bondFunctional"].value, /^[a-z ]{4,40}$/i, "Enquadramento funcional inválido.")
+	&& validate(form["bondTime"].value, /^[0-9]{1,2}$/i, "Carga horária inválida.")
 	&& validate(form["bondExclusive"].value, null, "Dedicação exclusiva inválida.")
     
-        && validate(form["periodMonth"].value, /[0-9]{1,2}$/i, "Mês de início inválido.")
-        && validate(form["periodYear"].value, /[0-9]{2,4}$/i, "Ano de início inválido.")
+        && validate(form["periodMonth"].value, /^[0-9]{1,2}$/i, "Mês de início inválido.")
+        && validate(form["periodYear"].value, /^[0-9]{2,4}$/i, "Ano de início inválido.")
         
         && validate(form["otherInfo"].value, /.{0,300}$/i, "Outras informações inválido.");
 }
