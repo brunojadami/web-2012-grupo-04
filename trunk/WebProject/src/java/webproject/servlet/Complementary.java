51ebaf2b782c3bd5df6dfce8b2021e649fd5654f
lucasjadami@gmail.com <lucasjadami@gmail.com@099c295c-9551-93b2-87df-dac76434ca7d>
2012-10-18 21:05:40 +0000

    Refactored a package.

---
+++ trunk/WebProject/src/java/webproject/servlet/Complementary.java
@@ -0,0 +1,104 @@
+package webproject.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import webproject.misc.Util;
+import webproject.validation.Validator;
+
+/**
+ * Complementary servlet.
+ */
+@WebServlet(name = "Complementary", urlPatterns =
+{
+    "/Complementary"
+})
+public class Complementary extends HttpServlet
+{
+    /**
+     * Processa a requisição.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String generalLevel = (String) request.getParameter("generalLevel");
+        String generalInstitution = (String) request.getParameter("generalInstitution");
+        String generalCourse = (String) request.getParameter("generalCourse");
+        String generalStatus = (String) request.getParameter("generalStatus");
+	String generalTime = (String) request.getParameter("generalTime");
+        String periodBegin = (String) request.getParameter("periodBegin");
+        String periodEnd = (String) request.getParameter("periodEnd");
+        String scholarshipPresence = (String) request.getParameter("scholarshipPresence");
+        String scholarshipAgency = (String) request.getParameter("scholarshipAgency");
+        
+        // Nota: a validação no servidor ainda não é feita nessa parte do trabalho.
+        // A única coisa validada aqui são os combo boxes.
+        Validator validator = new Validator();
+        
+        String validatorMessage = validator.validateListOption(generalLevel, Util.getComplementaryLevelNames(), "Nível inválido.");
+        validatorMessage = validatorMessage == null 
+                ? validator.validateListOption(generalStatus, Util.getAcademicStatusNames(), "Estado inválido.") : validatorMessage;
+	validatorMessage = validatorMessage == null 
+                ? validator.validateYesNoOption(scholarshipPresence, "Com bolsa? inválido.") : validatorMessage;
+    
+        RequestDispatcher dispatcher;
+        
+        // OBS: há uma certa 'repetição' de comandos. Isso será removido quando o 'Model' for implementado.
+        if (validatorMessage == null)
+        {
+            dispatcher = request.getRequestDispatcher("show_operation.jsp");
+            request.setAttribute("Attribute:00.Nível", generalLevel);
+            request.setAttribute("Attribute:01.Instituição", generalInstitution);
+            request.setAttribute("Attribute:02.Curso", generalCourse);
+            request.setAttribute("Attribute:03.Estado", generalStatus);
+	    request.setAttribute("Attribute:04.Carga horária", generalTime);
+            request.setAttribute("Attribute:05.Ano de início", periodBegin);
+            request.setAttribute("Attribute:06.Ano de término", periodEnd);
+            request.setAttribute("Attribute:07.Com bolsa?", scholarshipPresence);
+            request.setAttribute("Attribute:08.Agência", scholarshipAgency);
+        }
+        else
+        {
+            dispatcher = request.getRequestDispatcher("complementary.jsp");
+            request.setAttribute("message", validatorMessage);
+        }
+        
+        dispatcher.forward(request, response);
+    }
+
+    /**
+     * Método GET.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+
+    /**
+     * Método POST.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+}
---
+++ trunk/WebProject/src/java/webproject/servlet/Production.java
@@ -0,0 +1,116 @@
+package webproject.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import webproject.misc.Util;
+import webproject.validation.Validator;
+
+/**
+ * Production servlet.
+ */
+@WebServlet(name = "Production", urlPatterns =
+{
+    "/Production"
+})
+public class Production extends HttpServlet
+{
+    /**
+     * Processa a requisição.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String completeName = (String) request.getParameter("completeName");
+        String generalTitle = (String) request.getParameter("generalTitle");
+        String generalLanguage = (String) request.getParameter("generalLanguage");
+        String generalYear = (String) request.getParameter("generalYear");
+        String detailedTitle = (String) request.getParameter("detailedTitle");
+        String detailedISSN = (String) request.getParameter("detailedISSN");
+        String otherInfo = (String) request.getParameter("otherInfo");
+        String bookType = (String) request.getParameter("bookType");
+        String bookISSN = (String) request.getParameter("bookISSN");
+        String formType = (String) request.getParameter("formType");
+        
+        Validator validator = new Validator();
+	String validatorMessage = validator.validateProductionFormType(formType, "Tipo do formulário inválido.");
+	
+	if (formType.equals("accepted"))
+	{
+	    validatorMessage = validatorMessage == null 
+                ? validator.validateListOption(generalLanguage, Util.getLanguages(), "Idioma inválido.") : validatorMessage;
+	}
+	else if (formType.equals("book"))
+	{
+	    validatorMessage = validatorMessage == null 
+                ? validator.validateListOption(bookType, Util.getProductionTypes(), "Tipo de produção inválido.") : validatorMessage;
+	}
+    
+        RequestDispatcher dispatcher;
+        if (validatorMessage == null)
+        {
+            dispatcher = request.getRequestDispatcher("show_operation.jsp");
+	    if (formType.equals("published"))
+	    {
+		request.setAttribute("Attribute:00.DOI, ISSN ou parte do nome do periódico", completeName);
+	    }
+	    else if (formType.equals("accepted"))
+	    {
+		request.setAttribute("Attribute:00.Título", generalTitle);
+		request.setAttribute("Attribute:01.Idioma", generalLanguage);
+		request.setAttribute("Attribute:02.Ano previsto para publicação", generalYear);
+		request.setAttribute("Attribute:03.Título do periódico/revista em que o artigo será publicado", detailedTitle);
+		request.setAttribute("Attribute:04.ISSN", detailedISSN);
+		request.setAttribute("Attribute:05.Outras informações", otherInfo);
+	    }
+	    else if (formType.equals("book"))
+	    {
+		request.setAttribute("Attribute:00.Tipo de produção", bookType);
+		request.setAttribute("Attribute:01.DOI ou ISSN", bookISSN);
+	    }
+        }
+        else
+        {
+            dispatcher = request.getRequestDispatcher("production.jsp");
+            request.setAttribute("message", validatorMessage);
+        }
+        
+        dispatcher.forward(request, response);
+    }
+
+    /**
+     * Método GET.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+
+    /**
+     * Método POST.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+}
