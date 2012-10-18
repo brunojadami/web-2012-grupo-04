fd6b46084e27eb86611f90d6f2fcda0c273a69ec
lucasjadami@gmail.com <lucasjadami@gmail.com@099c295c-9551-93b2-87df-dac76434ca7d>
2012-10-18 20:59:54 +0000

    Added login bean.

---
+++ trunk/WebProject/src/java/webproject/bean/Login.java
@@ -0,0 +1,31 @@
+package webproject.bean;
+
+/**
+ * Essa classe representa o bean Login.
+ */
+public class Login extends Bean
+{
+    private String email;
+    
+    private String password;
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
