473704ba8849cddf4cd38441133379819ce6e9a5
brunojadami@gmail.com <brunojadami@gmail.com@099c295c-9551-93b2-87df-dac76434ca7d>
2012-10-26 00:14:52 +0000

    Added missing beans.

---
+++ trunk/WebProject/src/java/webproject/bean/Complementary.java
@@ -0,0 +1,109 @@
+package webproject.bean;
+
+public class Complementary extends Bean
+{
+
+    private String generalLevel;
+    private String generalInstitution;
+    private String generalCourse;
+    private String generalStatus;
+    private String generalTime;
+    private String periodBegin;
+    private String periodEnd;
+    private String scholarshipPresence;
+    private String scholarshipAgency;
+
+    public Complementary()
+    {
+    }
+
+    public String getGeneralLevel()
+    {
+	return generalLevel;
+    }
+
+    public void setGeneralLevel(String generalLevel)
+    {
+	this.generalLevel = generalLevel;
+    }
+
+    public String getGeneralInstitution()
+    {
+	return generalInstitution;
+    }
+
+    public void setGeneralInstitution(String generalInstitution)
+    {
+	this.generalInstitution = generalInstitution;
+    }
+
+    public String getGeneralCourse()
+    {
+	return generalCourse;
+    }
+
+    public void setGeneralCourse(String generalCourse)
+    {
+	this.generalCourse = generalCourse;
+    }
+
+    public String getGeneralStatus()
+    {
+	return generalStatus;
+    }
+
+    public void setGeneralStatus(String generalStatus)
+    {
+	this.generalStatus = generalStatus;
+    }
+
+    public String getGeneralTime()
+    {
+	return generalTime;
+    }
+
+    public void setGeneralTime(String generalTime)
+    {
+	this.generalTime = generalTime;
+    }
+
+    public String getPeriodBegin()
+    {
+	return periodBegin;
+    }
+
+    public void setPeriodBegin(String periodBegin)
+    {
+	this.periodBegin = periodBegin;
+    }
+
+    public String getPeriodEnd()
+    {
+	return periodEnd;
+    }
+
+    public void setPeriodEnd(String periodEnd)
+    {
+	this.periodEnd = periodEnd;
+    }
+
+    public String getScholarshipPresence()
+    {
+	return scholarshipPresence;
+    }
+
+    public void setScholarshipPresence(String scholarshipPresence)
+    {
+	this.scholarshipPresence = scholarshipPresence;
+    }
+
+    public String getScholarshipAgency()
+    {
+	return scholarshipAgency;
+    }
+
+    public void setScholarshipAgency(String scholarshipAgency)
+    {
+	this.scholarshipAgency = scholarshipAgency;
+    }
+}
---
+++ trunk/WebProject/src/java/webproject/bean/Experience.java
@@ -0,0 +1,120 @@
+package webproject.bean;
+
+public class Experience extends Bean
+{
+
+    private String profInstitution;
+    private String bondType;
+    private String bondEmployment;
+    private String bondFunctional;
+    private String bondTime;
+    private String bondExclusive;
+    private String periodMonth;
+    private String periodFinished;
+    private String periodYear;
+    private String otherInfo;
+
+    public Experience()
+    {
+    }
+
+    public String getProfInstitution()
+    {
+	return profInstitution;
+    }
+
+    public void setProfInstitution(String profInstitution)
+    {
+	this.profInstitution = profInstitution;
+    }
+
+    public String getBondType()
+    {
+	return bondType;
+    }
+
+    public void setBondType(String bondType)
+    {
+	this.bondType = bondType;
+    }
+
+    public String getBondEmployment()
+    {
+	return bondEmployment;
+    }
+
+    public void setBondEmployment(String bondEmployment)
+    {
+	this.bondEmployment = bondEmployment;
+    }
+
+    public String getBondFunctional()
+    {
+	return bondFunctional;
+    }
+
+    public void setBondFunctional(String bondFunctional)
+    {
+	this.bondFunctional = bondFunctional;
+    }
+
+    public String getBondTime()
+    {
+	return bondTime;
+    }
+
+    public void setBondTime(String bondTime)
+    {
+	this.bondTime = bondTime;
+    }
+
+    public String getBondExclusive()
+    {
+	return bondExclusive;
+    }
+
+    public void setBondExclusive(String bondExclusive)
+    {
+	this.bondExclusive = bondExclusive;
+    }
+
+    public String getPeriodMonth()
+    {
+	return periodMonth;
+    }
+
+    public void setPeriodMonth(String periodMonth)
+    {
+	this.periodMonth = periodMonth;
+    }
+
+    public String getPeriodFinished()
+    {
+	return periodFinished;
+    }
+
+    public void setPeriodFinished(String periodFinished)
+    {
+	this.periodFinished = periodFinished;
+    }
+
+    public String getPeriodYear()
+    {
+	return periodYear;
+    }
+
+    public void setPeriodYear(String periodYear)
+    {
+	this.periodYear = periodYear;
+    }
+
+    public String getOtherInfo()
+    {
+	return otherInfo;
+    }
+
+    public void setOtherInfo(String otherInfo)
+    {
+	this.otherInfo = otherInfo;
+    }
+}
