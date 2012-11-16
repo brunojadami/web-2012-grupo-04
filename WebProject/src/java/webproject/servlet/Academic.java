ec39b84b91153fc620cecf96777b6a323c1ac854
lucasjadami@gmail.com <lucasjadami@gmail.com@099c295c-9551-93b2-87df-dac76434ca7d>
2012-11-16 14:58:32 +0000

    Added Hibernate to the academic page.

--- WebProject/src/java/webproject/servlet/Academic.java
+++ WebProject/src/java/webproject/servlet/Academic.java
@@ -1,14 +1,15 @@
 package webproject.servlet;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import webproject.bean.Bean;
-import webproject.misc.BeanIO;
+import org.hibernate.Session;
+import webproject.misc.HibernateUtil;
 import webproject.misc.Util;
 import webproject.validation.Validator;
 
@@ -34,9 +35,8 @@ public class Academic extends HttpServlet
         String action = request.getParameter("action");
         
         RequestDispatcher dispatcher = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
         
-        // Nota: toda a parte de Bean, IO e suas respectivas decisões de projeto
-        // estão descritas nas classes Bean.java e BeanIO.java.
         webproject.bean.Academic academic = new webproject.bean.Academic();
         academic.setId(Integer.parseInt(request.getParameter("id")));
         
@@ -47,26 +47,24 @@ public class Academic extends HttpServlet
         }
         else if (action.equals("update"))
         {
-            academic.setGeneralLevel(Bean.createField("Nível", 0, request.getParameter("generalLevel")));
-            academic.setGeneralType(Bean.createField("Tipo", 1, request.getParameter("generalType")));
-            academic.setGeneralInstitute(Bean.createField("Instituto", 2, request.getParameter("generalInstitute")));
-            academic.setGeneralCourse(Bean.createField("Curso", 3, request.getParameter("generalCourse")));
-            academic.setGeneralStatus(Bean.createField("Estado", 4, request.getParameter("generalStatus")));
-            academic.setPeriodStartDate(Bean.createField("Data de início", 5, request.getParameter("periodStartDate")));
-            academic.setPeriodEndDate(Bean.createField("Data de término", 6, request.getParameter("periodEndDate")));
-            academic.setScholarshipPresence(Bean.createField("Com bolsa?", 7, request.getParameter("scholarshipPresence")));
-            academic.setScholarshipAgency(Bean.createField("Agência", 8, request.getParameter("scholarshipAgency")));
-            academic.setMiscInfo(Bean.createField("Outros", 9, request.getParameter("miscInfo")));
+            academic.setGeneralLevel(request.getParameter("generalLevel"));
+            academic.setGeneralType(request.getParameter("generalType"));
+            academic.setGeneralInstitute(request.getParameter("generalInstitute"));
+            academic.setGeneralCourse(request.getParameter("generalCourse"));
+            academic.setGeneralStatus(request.getParameter("generalStatus"));
+            academic.setPeriodStartDate(request.getParameter("periodStartDate"));
+            academic.setPeriodEndDate(request.getParameter("periodEndDate"));
+            academic.setScholarshipPresence(request.getParameter("scholarshipPresence"));
+            academic.setScholarshipAgency(request.getParameter("scholarshipAgency"));
+            academic.setMiscInfo(request.getParameter("miscInfo"));
 
-            // Nota: a validação no servidor ainda não é feita nessa parte do trabalho.
-            // A única coisa validada aqui são os combo boxes.
             Validator validator = new Validator();
 
-            String validatorMessage = validator.validateListOption(Bean.getFieldValue(academic.getGeneralLevel()), Util.getAcademicLevelNames(), "Nível inválido.");
+            String validatorMessage = validator.validateListOption(academic.getGeneralLevel(), Util.getAcademicLevelNames(), "Nível inválido.");
             validatorMessage = validatorMessage == null 
-                    ? validator.validateListOption(Bean.getFieldValue(academic.getGeneralStatus()), Util.getAcademicStatusNames(), "Estado inválido.") : validatorMessage;
+                    ? validator.validateListOption(academic.getGeneralStatus(), Util.getAcademicStatusNames(), "Estado inválido.") : validatorMessage;
             validatorMessage = validatorMessage == null 
-                    ? validator.validateYesNoOption(Bean.getFieldValue(academic.getScholarshipPresence()), "Com bolsa? inválida.") : validatorMessage; 
+                    ? validator.validateYesNoOption(academic.getScholarshipPresence(), "Com bolsa? inválida.") : validatorMessage; 
 
             if (validatorMessage == null)
             {
@@ -75,14 +73,8 @@ public class Academic extends HttpServlet
                 request.setAttribute("message", "Operação realizada com sucesso");
                 request.setAttribute("servletName", "Academic");
 
-                try
-                {
-                    BeanIO.getInstance().save(academic);
-                }
-                catch (Exception ex)
-                {
-                    throw new ServletException(ex);
-                }
+                session.save(academic);
+                session.close();
             }
             else
             {
@@ -93,34 +85,24 @@ public class Academic extends HttpServlet
         }
         else if (action.equals("view"))
         {
-            try
-            {
-                dispatcher = request.getRequestDispatcher("show_bean.jsp");
-                request.setAttribute("bean", BeanIO.getInstance().load(academic));
-                request.setAttribute("message", "Visualizar registro acadêmico");
-                request.setAttribute("servletName", "Academic");
-            }
-            catch (Exception ex)
-            {
-                throw new ServletException(ex);
-            }
+            dispatcher = request.getRequestDispatcher("show_bean.jsp");
+            request.setAttribute("bean", session.load(webproject.bean.Academic.class, academic.getId()));
+            request.setAttribute("message", "Visualizar registro acadêmico");
+            request.setAttribute("servletName", "Academic");
         }
         else if (action.equals("list_view"))
         {
-            try
-            {
-                dispatcher = request.getRequestDispatcher("show_beans.jsp");
-                request.setAttribute("list", BeanIO.getInstance().loadAll(academic.getClass()));
-                request.setAttribute("message", "Visualizar registros acadêmicos");
-                request.setAttribute("servletName", "Academic");
-            }
-            catch (Exception ex)
-            {
-                throw new ServletException(ex);
-            }
+            List<webproject.bean.Academic> academics = session.createQuery("from Academic").list();
+            session.close();
+            
+            dispatcher = request.getRequestDispatcher("show_beans.jsp");
+            request.setAttribute("list", academics);
+            request.setAttribute("message", "Visualizar registros acadêmicos");
+            request.setAttribute("servletName", "Academic");
         }
         
         dispatcher.forward(request, response);
+        session.close();
     }
 
     /**
